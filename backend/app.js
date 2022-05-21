d3e851421268ae8f5abea264aebc06748f313bac
Tibo Vanheule <tibo@tibovanheule.space>
2022-05-21 23:07:51 +0200

    register user en usage of db

--- backend/app.js
+++ backend/app.js
@@ -19,8 +19,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 let db_url = process.env.DB_URL || "data.db";
+
+
+const createTables = (newdb) => newdb.exec("", ()  => { });
+
 function createDatabase() {
-    var newdb = new sqlite3.Database(db_url, (err) => {
+    let newdb = new sqlite3.Database(db_url, (err) => {
         if (err) {
             console.log("Getting error " + err);
             exit(1);
@@ -29,34 +33,7 @@ function createDatabase() {
     });
 }
 
-function createTables(newdb) {
-    newdb.exec(`
-    create table hero (
-        hero_id int primary key not null,
-        hero_name text not null,
-        is_xman text not null,
-        was_snapped text not null
-    );
-    insert into hero (hero_id, hero_name, is_xman, was_snapped)
-        values (1, 'Spiderman', 'N', 'Y'),
-               (2, 'Tony Stark', 'N', 'N'),
-               (3, 'Jean Grey', 'Y', 'N');
-
-    create table hero_power (
-        hero_id int not null,
-        hero_power text not null
-    );
-
-    insert into hero_power (hero_id, hero_power)
-        values (1, 'Web Slinging'),
-               (1, 'Super Strength'),
-               (1, 'Total Nerd'),
-               (2, 'Total Nerd'),
-               (3, 'Telepathic Manipulation'),
-               (3, 'Astral Projection');
-        `, ()  => { });
-}
-new sqlite3.Database(db_url, sqlite3.OPEN_READWRITE, (err) => {
+const startDB = () => new sqlite3.Database(db_url, sqlite3.OPEN_READWRITE, (err) => {
     if (err && err.code == "SQLITE_CANTOPEN") {
         createDatabase();
         return;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,28 +1,40 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const jwt = require('jsonwebtoken');
 const crypt = require('sha512crypt-node');
-var sqlite3 = require('sqlite3');
+const sqlite3 = require('sqlite3');
+const validator = require('validator');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    let hash = crypt.sha512crypt("pass","saltsalt");
-  res.send(hash);
-});
-
-router.post("/generateToken", (req, res) => {
+router.post("/register", (req, res) => {
   // Validate User Here
   // Then generate JWT Token
+  let db_url = process.env.DB_URL || "data.db";
+  let newdb = new sqlite3.Database(db_url, (err) => {
+    if (err) res.send("error has occured");
+  });
+  let json = req.body
 
-  let jwtSecretKey = process.env.JWT_SECRET_KEY;
-  let data = {
-    time: Date(),
-    userId: 12,
+  if (!validator.isEmail(json.email)){
+    res.send({"error":"invalid email"})
   }
+  let hash = crypt.sha512crypt(json.password,"saltsalt");
+  newdb.exec("insert into users (mail,first_naam,last_name,home,admin, hash) values ('"+json.email+"','tibo','vanheule',0,0,'"+hash+"')", (r,err)  => {
+    if (err) {
+      res.send(err);
+      return;
+    }
+    let jwtSecretKey = process.env.JWT_SECRET_KEY;
+    let data = {
+      time: Date(),
+      userId: 1,
+    }
 
-  const token = jwt.sign(data, jwtSecretKey);
+    const token = jwt.sign(data, jwtSecretKey);
 
-  res.send(token);
+    res.send(token);
+  });
+
+
 });
 
 router.get("/user/validateToken", (req, res) => {
